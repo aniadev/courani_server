de33b4178436cf3b3b53c85e2828222b399d5908
phamhai-laptop <phamcong.hai.90@gmail.com>
2021-09-06 09:41:48 +0700

    fix status code

--- routes/course.js
+++ routes/course.js
@@ -11,7 +11,7 @@ router.post("/create", verifyToken, async (req, res) => {
 
   if (!title) {
     return res
-      .status(400)
+      .status(200)
       .json({ success: false, message: "Title must be specified" });
   }
 
@@ -111,7 +111,7 @@ router.delete("/:id", verifyToken, async (req, res) => {
     });
     // UserID not auth:
     if (!deletedCourse) {
-      return res.status(401).json({
+      return res.status(200).json({
         success: false,
         message: "Course not found or not authenticated user",
       });
